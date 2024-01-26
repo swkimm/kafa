7dcb88ba050795a7e89a05832a0c8c53a606ac73
jspark2000 <idpjs2000@gmail.com>
2024-01-26 11:29:40 +0000

    fix: modify association page

--- frontend/src/pages/association/items/GreetingItem.tsx
+++ frontend/src/pages/association/items/GreetingItem.tsx
@@ -1,16 +1,22 @@
 export const GreetingItem = () => {
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-10 lg:px-20">
-      <h3 className="text-lg font-semibold leading-7 text-gray-900">인사말</h3>
-      <p className="mt-1 max-w-2xl text-base leading-6 text-gray-500">
-        대한미식축구협회장 인사말
-      </p>
-      <div className="mt-5">
-        <img
-          className="h-ull w-full rounded-lg object-cover"
-          src="/association/greeting_image.jpeg"
-          alt=""
-        />
+      <div className="px-4 sm:px-0">
+        <h3 className="text-lg font-semibold leading-7 text-gray-900">
+          인사말
+        </h3>
+        <p className="mt-1 max-w-2xl text-base leading-6 text-gray-500">
+          대한미식축구협회장 인사말
+        </p>
+      </div>
+      <div className="mt-6 px-4 sm:px-0">
+        <div className="divide-y divide-gray-200">
+          <img
+            className="h-ull w-full rounded-lg object-cover"
+            src="/association/greeting_image.jpeg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   )
--- frontend/src/pages/association/items/OrganizationItem.tsx
+++ frontend/src/pages/association/items/OrganizationItem.tsx
@@ -1,12 +1,18 @@
 const OrganizationItem = () => {
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-10 lg:px-20">
-      <h3 className="text-lg font-semibold leading-7 text-gray-900">조직도</h3>
-      <p className="mt-1 max-w-2xl text-base leading-6 text-gray-500">
-        대한미식축구협회 조직도
-      </p>
-      <div className="mt-5 rounded-lg bg-white p-10">
-        <img src="/association/org.jpg" alt="" />
+      <div className="px-4 sm:px-0">
+        <h3 className="text-lg font-semibold leading-7 text-gray-900">
+          조직도
+        </h3>
+        <p className="mt-1 max-w-2xl text-base leading-6 text-gray-500">
+          대한미식축구협회 조직도
+        </p>
+      </div>
+      <div className="mt-6 px-4 sm:px-0">
+        <div className="divide-y divide-gray-200">
+          <img src="/association/org.jpg" alt="" />
+        </div>
       </div>
     </div>
   )
