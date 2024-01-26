7b7796298b976cfc2265cf99ee5b2bc46417c1fb
jspark2000 <idpjs2000@gmail.com>
2024-01-26 11:17:49 +0000

    fix: modify favicon

--- frontend/src/commons/layout/ConsoleLayout.tsx
+++ frontend/src/commons/layout/ConsoleLayout.tsx
@@ -1,7 +1,14 @@
 import Sidebar from '@/components/sidebar/Sidebar'
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 
 const ConsoleLayout: React.FC = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
+
   return (
     <div className="flex min-h-screen w-full bg-gray-100/90">
       <Sidebar>
--- frontend/src/commons/layout/MainLayout.tsx
+++ frontend/src/commons/layout/MainLayout.tsx
@@ -1,11 +1,16 @@
 import Footer from '@/components/footer/Footer'
 import Header from '@/components/header/Header'
+import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
 
 const MainLayout: React.FC = () => {
   const location = useLocation()
   const isHomePage = location.pathname === '/'
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
 
   return (
     <div className="flex min-h-screen flex-col bg-slate-50">
